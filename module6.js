390efa8ebc50e7dd9d8eed2eacec9a64e2425fc3
fuczak <prus.damian@gmail.com>
2015-01-26 16:02:23 +0100

    added exercise 7 and commented code in exercise 6

--- module6.js
+++ module6.js
@@ -2,14 +2,17 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (directory, filter, callback) {
+	console.log('module function init');
 	fs.readdir(directory, function (err, list) {
+		console.log('module readdir completed')
 		if (err)
 			return callback(err)
-
+		console.log('modeule readdir no error')
 		list = list.filter(function (file) {
+			console.log('module list filter applied')
 			return path.extname(file) === '.' + filter
 		})
-
-		callback(null, list)
+		console.log('module callback with null')
+		callback(null, list)		
 	})
 };
