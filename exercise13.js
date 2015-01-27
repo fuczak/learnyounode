cff12483b93dae9865241335d65d4e09016d4d3c
fuczak <prus.damian@gmail.com>
2015-01-27 11:12:03 +0100

    finished course

--- exercise13.js
+++ exercise13.js
@@ -3,13 +3,60 @@ var url = require('url')
 
 function mySolution() {
 	var server = http.createServer(function (req, res) {
-		if(req.method != 'GET')
-			return res.end('I only accept GET requests')
-
-		//todo
+		var request = url.parse(req.url, true)
+		var time = new Date(request.query.iso)
+		
+		if(request.pathname == '/api/parsetime') {
+			res.writeHead(200, { 'Content-Type': 'application/json' })
+			res.end(JSON.stringify({
+				"hour": time.getHours(),
+				"minute": time.getMinutes(),
+				"second": time.getSeconds()
+			}))		
+		} else if(request.pathname == '/api/unixtime') {
+			res.writeHead(200, { 'Content-Type': 'application/json' })
+			res.end(JSON.stringify({
+				"unixtime": time.getTime()
+			}))
+		}
 	})
 
 	server.listen(process.argv[2])
 }
 
+function officialSolution() {
+		function parsetime (time) {
+	  		return {
+			    hour: time.getHours(),
+			    minute: time.getMinutes(),
+			    second: time.getSeconds()
+	  		}
+		}
+
+		function unixtime (time) {
+			return { unixtime : time.getTime() }
+		}
+
+		var server = http.createServer(function (req, res) {
+	  		var parsedUrl = url.parse(req.url, true)
+	  		var time = new Date(parsedUrl.query.iso)
+	  		var result
+
+	  		if (/^\/api\/parsetime/.test(req.url))
+	    		result = parsetime(time)
+	  		else if (/^\/api\/unixtime/.test(req.url))
+	    		result = unixtime(time)
+
+	  		if (result) {
+	    		res.writeHead(200, { 'Content-Type': 'application/json' })
+	    		res.end(JSON.stringify(result))
+	  		} else {
+	    		res.writeHead(404)
+	    		res.end()
+	  		}
+		})
+		server.listen(Number(process.argv[2]))
+}
+
 mySolution();
+//officialSolution();
